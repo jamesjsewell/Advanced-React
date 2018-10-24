0a02952377df2cbcb1ed9d71e2ae2e8c0ae159a0
james sewell <jamesjosephsewell@gmail.com>
2018-10-24 11:02:23 -0500

    done with mod 2

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,15 +1,29 @@
 import Link from 'next/link'
+import NavStyles from './styles/NavStyles'
 
 const Nav = () => (
-  <div>
+  <NavStyles>
     <Link href='/'>
-      <a>home</a>
+      <a>Home</a>
     </Link>
 
     <Link href='/sell'>
-      <a>sell</a>
+      <a>Sell</a>
     </Link>
-  </div>
+
+    <Link href='/signup'>
+      <a>Signup</a>
+    </Link>
+
+    <Link href='/orders'>
+      <a>Orders</a>
+    </Link>
+
+    <Link href='/account'>
+      <a>Me</a>
+    </Link>
+
+  </NavStyles>
 
 )
 
