3b4d5754c5f796785731f46759350fa5c445126e
james sewell <jamesjosephsewell@gmail.com>
2018-11-05 13:10:55 -0600

    got user register working on the frontend 03:52 of video 26

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -7,6 +7,12 @@ const db = require('./db')
 const server = createServer()
 
 server.express.use(cookieParser())
+// decode the jwt so we can get the user id from the request
+server.express.use((req, res, next)=>{
+  const { sick_fits_token } = req.cookies
+  console.log(sick_fits_token, 'TOKEN') 
+  next()
+})
 // TODO use express middleware to handle cookies ( jwt )
 // TODO use express middleware to populate current user
 
